54b519e562296480ba10aefa45818fdcc473aa28
Daniel Snowden <danielsnowdenn@gmail.com>
2025-06-28 03:07:14 -0400

    fixed proxy gen and improved interface

--- proxymain.py
+++ proxymain.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -14,19 +15,37 @@ class Colors:
     CYAN = Fore.CYAN
     RESET = Style.RESET_ALL
 
-ascii_art = f"""
+user_agents = ["Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+               "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:70.0) Gecko/20100101 Firefox/70.0"]
 
-   ▄█    █▄       ▄████████ ▀████    ▐████▀ ▀████    ▐████▀ ▀████    ▐████▀ 
-  ███    ███     ███    ███   ███▌   ████▀    ███▌   ████▀    ███▌   ████▀  
-  ███    ███     ███    ███    ███  ▐███       ███  ▐███       ███  ▐███    
- ▄███▄▄▄▄███▄▄   ███    ███    ▀███▄███▀       ▀███▄███▀       ▀███▄███▀    
-▀▀███▀▀▀▀███▀  ▀███████████    ████▀██▄        ████▀██▄        ████▀██▄     
-  ███    ███     ███    ███   ▐███  ▀███      ▐███  ▀███      ▐███  ▀███    
-  ███    ███     ███    ███  ▄███     ███▄   ▄███     ███▄   ▄███     ███▄  
-  ███    █▀      ███    █▀  ████       ███▄ ████       ███▄ ████       ███▄ 
-              
-- version: 1.0
-- github: https://github.com/emptyhax
+
+
+
+ascii_art = f"""                                                                      
+
+                                                 
+              ▒▒              ▒▒                              
+            ▒▒░░▒▒▒▒      ▒▒▒▒░░▒▒                            
+          ▓▓░░  ░░░░▒▒▓▓▒▒░░░░  ░░▒▒                          
+          ▓▓░░  ░░░░░░░░░░░░░░  ░░▒▒                          
+          ▒▒░░    ░░░░░░░░░░    ░░▒▒                          
+          ▒▒░░  ░░░░░░░░░░░░░░  ░░▒▒                          
+            ▒▒░░░░  ░░░░░░  ░░░░▒▒                            
+          ▒▒░░░░      ░░    ░░░░▒▒▒▒░░    ▒▒                  
+          ▒▒░░    ██      ▓▓    ▒▒▓▓      ▒▒       _    _       _  __   _____                     
+        ▒▒░░░░                  ░░░░▒▒    ▒▒  ▒▒  | |  | |     | |/ _| | ___ \                            
+          ▒▒░░      ██████      ░░▒▒    ▒▒░░  ▒▒  | |  | | ___ | | |_  | |_/ / __ _____  ___   _          
+            ▒▒░░      ██      ▒▒▓▓    ▒▒░░░░  ░░  | |/\| |/ _ \| |  _| |  __/ '__/ _ \ \/ / | | |          
+          ▒▒░░▒▒░░          ░░▒▒░░▒▒▒▒░░░░░░▒▒    \  /\  / (_) | | |   | |  | | | (_) >  <| |_| |          
+          ▒▒  ░░              ▓▓░░░░▒▒▒▒▒▒▒▒░░     \/  \/ \___/|_|_|   \_|  |_|  \___/_/\_\\__, |          
+        ▒▒░░                ░░░░░░░░░░░░▒▒                                                  __/ |          
+        ▒▒▒▒▒▒            ░░░░▒▒▒▒▒▒░░░░▒▒                                                 |___/        
+        ▒▒░░░░▓▓      ▒▒  ░░░░▒▒░░░░░░░░▒▒                    
+        ▒▒░░░░▒▒      ▒▒  ░░░░▒▒  ░░░░░░▒▒                    
+        ▒▒░░    ▓▓  ▒▒      ░░▒▒      ▒▒     - version: 1.1                
+          ▒▒      ▒▒          ▒▒  ▒▒  ▒▒     - github: https://github.com/emptyhax/proxy-hax                
+        ▒▒  ▒▒  ▒▒  ▒▒  ▒▒  ▒▒▒▒▒▒▒▒▒▒       - made by hax and dan
+        ▒▒▓▓▒▒▒▒    ▓▓▒▒▒▒▒▒        
 
 """
 
@@ -35,9 +54,15 @@ print(Colors.CYAN + ascii_art)
 valid_proxy_file = "./valid_proxy.txt"
 URL = "https://www.us-proxy.org/"
 
+if os.path.exists(valid_proxy_file):
+    os.remove(valid_proxy_file)
+
+
 def collect_proxies(url):
     print(Colors.YELLOW + "[*] Fetching proxy list...")
-    response = requests.get(url)
+    headers = {'User-Agent':
+    random.choice(user_agents)}
+    response = requests.get(url,headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     table = soup.find('table', {'class': 'table table-striped table-bordered'})
@@ -47,16 +72,19 @@ def collect_proxies(url):
         cols = row.find_all('td')
         ip = cols[0].text
         port = cols[1].text
-        proxies.append(f"{ip}:{port}")
-    
+        if port in ['80', '8080', '3128']:
+            proxies.append(f"{ip}:{port}")
+
     return proxies
 
 def test_proxy(proxy):
-    url = "http://www.google.com"
+    url = "https://httpbin.org/ip"
     proxies = {"http": f"http://{proxy}", "https": f"http://{proxy}"}
 
     try:
-        response = requests.get(url, proxies=proxies, timeout=3)
+        headers = {'User-Agent':
+                   random.choice(user_agents)}
+        response = requests.get(url,proxies=proxies, headers=headers, timeout=5)
         if response.status_code == 200:
             print(Colors.GREEN + f"[+] Valid proxy: {proxy}")
             with open(valid_proxy_file, 'a') as file:
@@ -76,7 +104,7 @@ def main():
     valid_proxies = []
 
     for proxy in proxies:
-        time.sleep(random.randint(1, 2))
+        time.sleep(random.uniform(1, 2))
         if test_proxy(proxy):
             valid_proxies.append(proxy)
 
@@ -86,4 +114,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
